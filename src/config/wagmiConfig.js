87723d92d091333db484fe07d48e2eec28d744b4
Alabi Daniel <alabidaniel719@gmail.com>
2025-06-30 20:45:40 +0100

    Add .env file and fix WalletConnect config

--- src/config/wagmiConfig.js
+++ src/config/wagmiConfig.js
@@ -2,8 +2,8 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { sepolia } from 'wagmi/chains';
 
 const config = getDefaultConfig({
-  appName: 'Wallet Dashboard',
-  projectId: '17a95eb8621774fd357283467c0d3c78',
+  appName: 'Wallet-Dashboard',
+  projectId: import.meta.env.VITE_PROJECT_ID, // ✅ Use env variable
   chains: [sepolia],
   ssr: false,
 });
